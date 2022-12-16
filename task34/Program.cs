1199ce1789d363e67a8698688ffc8334f72e8928
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-16 20:39:04 +0500

    Add new commit task34

---
+++ task34/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+//Проверяем на число
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Ввели некорректрое число");
+        }
+    }
+    return result;
+}
+
+//Получаем массив заполненный случайными положительными трёхзначными числами
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+    return array;
+}
+
+//распечатать массив на консоль
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i] + " "}");
+    }
+    Console.WriteLine();
+}
+
+
+
+//Проверка на чётность
+int GetParity(int[] array)
+{    
+    
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 ==  0)
+        {
+            count++;
+        }
+    }    
+    return count;
+}
+
+int dimension = GetNumber("Введите размерность массива: ");
+int[] array = InitArray(dimension);
+PrintArray(array);
+int count = GetParity(array);
+
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
